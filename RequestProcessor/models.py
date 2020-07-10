03e75c355ab9e092b969860fe6ec5614027ced9b
Behtarin Ferdousi <behtarin.ferdousi@tum.de>
2020-07-10 11:26:21 +0200

    Add Models and Migrate

---
+++ RequestProcessor/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    active = models.BooleanField(default=True)
+
+
+class HourlyStats(models.Model):
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(null=False)
+    request_count = models.BigIntegerField(null=False, default=0)
+    invalid_count = models.BigIntegerField(null=False, default=0)
+
+    class Meta:
+        unique_together = ('customer_id', 'datetime')
--- RequestProcessor/settings.py
+++ RequestProcessor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from RequestProcessor.auth import user,password
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'RequestProcessor',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'RequestProcessor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'request_processor',
+        'USER': user,
+        'PASSWORD': password,
+        'HOST': '',
+        'PORT': '',
     }
 }
 
